42535a60974677d666a25ec7ea7c654cb1511edb
joguerrero10 <joelg1014@hotmail.com>
2026-03-30 21:33:18 -0500

    Se creo la arquitectura y configuraciones de nestJS

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppConfigModule } from './config/config.module';
+
+@Module({
+  imports: [
+    AppConfigModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
